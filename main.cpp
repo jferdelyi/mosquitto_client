df5c9cd95f7f550ba3a9cdf32ff1a6561c74dbf0
Jean-François Erdelyi <jferdelyi@gmail.com>
2022-10-20 11:14:21 +0200

    [QA] Quality

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void signal_callback_handler(int) {
 	s_running = false;
 }
 
-int main(int argc, char *argv[]) {
+int main() {
 	signal(SIGINT, signal_callback_handler);
 
 	std::vector<std::string> l_topics = {"PUB0", "PUB1", "PUB2", "PUB3", "PUB4", "PUB5", "PUB6", "PUB7", "PUB8", "PUB9"};
@@ -21,12 +21,12 @@ int main(int argc, char *argv[]) {
 	int l_rc;
 
 	mosqpp::lib_init();
-	mqtt_client *iot_client = new mqtt_client("CLIENT", "localhost", 1883, 60);
+	mqtt_client *l_iot_client = new mqtt_client("CLIENT", "localhost", 1883, 60);
 	while (s_running) {
-		l_rc = iot_client->loop();
+		l_rc = l_iot_client->loop();
 		if (l_rc) {
 			l_subcribed = false;
-			iot_client->reconnect();
+			l_iot_client->reconnect();
 		} 
 		
 		if (!l_subcribed) {
@@ -34,21 +34,21 @@ int main(int argc, char *argv[]) {
 				// About message ID
 				// If not NULL, the function will set this to the message id of this particular message.  
 				// This can be then used with the publish callback to determine when the message has been sent.  
-				// Note that although the MQTT protocol doesn’t use message ids for messages with QoS=0, libmosquitto assigns them message ids so they can be tracked with this parameter.
-				iot_client->subscribe(NULL, l_topic.c_str());
-				iot_client->send_message(l_topic, "LISTENING");
+				// Note that although the MQTT protocol doesnâ€™t use message ids for messages with QoS=0, libmosquitto assigns them message ids so they can be tracked with this parameter.
+				l_iot_client->subscribe(NULL, l_topic.c_str());
+				l_iot_client->send_message(l_topic, "LISTENING");
 			}
 			l_subcribed = true;
 		} else {
 			for (const auto& l_topic : l_topics) {
-				iot_client->send_message(l_topic);
-				iot_client->loop_read();
+				l_iot_client->send_message(l_topic);
+				l_iot_client->loop_read();
 			}
 		}
 	}
 
-	delete iot_client;
-	iot_client = nullptr;
+	delete l_iot_client;
+	l_iot_client = nullptr;
 	mosqpp::lib_cleanup();
 
 	return EXIT_SUCCESS;
